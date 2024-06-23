5f07449cfd8385e14de5b6010e0538670450e98e
Justin <justinbansal@gmail.com>
2024-06-23 18:23:33 -0400

    Add assets for different routes

---
+++ animals.js
@@ -0,0 +1,16 @@
+const animals = [
+  { name: 'Cat', url: '/images/cat.jpg'},
+  { name: 'Dog', url: '/images/dog.jpg'},
+  { name: 'Lion', url: '/images/lion.jpg'},
+  { name: 'Monkey', url: '/images/monkey.jpg'},
+  { name: 'Sheep', url: '/images/sheep.jpg'},
+  { name: 'Crocodile', url: '/images/crocodile.jpg'},
+  { name: 'Alligator', url: '/images/alligator.jpg'},
+  { name: 'Fox', url: '/images/fox.jpg'},
+  { name: 'Bear', url: '/images/bear.jpg'},
+  { name: 'Bird', url: '/images/bird.jpg'},
+  { name: 'Rabbit', url: '/images/rabbit.jpg'},
+  { name: 'Horse', url: '/images/horse.jpg'},
+]
+
+export default animals;
---
+++ luxuryCarLogos.js
@@ -0,0 +1,52 @@
+const logos = [
+  {
+    name: "Mercedes",
+    url: "/images/mercedes.jpg"
+  },
+  {
+    name: "Audi",
+    url: "/images/audi.jpg"
+  },
+  {
+    name: "BMW",
+    url: "/images/bmw.jpg"
+  },
+  {
+    name: "Porsche",
+    url: "/images/porsche.jpeg"
+  },
+  {
+    name: "Lamborghini",
+    url: "/images/lamborghini.jpg"
+  },
+  {
+    name: "Aston Martin",
+    url: "/images/aston-martin.jpg"
+  },
+  {
+    name: "Lexus",
+    url: "/images/lexus.jpg"
+  },
+  {
+    name: "Cadillac",
+    url: "/images/cadillac.jpg"
+  },
+  {
+    name: "Bentley",
+    url: "/images/bentley/jpg"
+  },
+  {
+    name: "Maybach",
+    url: "/images/maybach.jpeg"
+  },
+  {
+    name: "Maserati",
+    url: "/images/maserati/jpg"
+  },
+  {
+    name: "Ferrari",
+    url: "/images/ferrari/jpg"
+  }
+]
+
+export default logos;
