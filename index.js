0361b129a41065d8aa4c45cd77816eafdc02f192
Justin <justinbansal@gmail.com>
2024-06-24 17:55:42 -0400

    Adds limit middleware to routes

--- index.js
+++ index.js
@@ -10,16 +10,32 @@ app.use(cors());
 
 app.use(express.static('public'));
 
+function limitMiddleware(req, res, next) {
+  let maxItems = req.query.limit || 5;
+  maxItems = parseInt(maxItems);
+
+  if (req.data && Array.isArray(req.data)) {
+    req.data = req.data.slice(0, maxItems);
+  }
+  next();
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.get('/luxury', (req, res) => {
-  res.send(logos);
+app.get('/luxury', (req, res, next) => {
+  req.data = logos;
+  next();
+}, limitMiddleware, (req, res) => {
+  res.json(req.data);
 })
 
-app.get('/animals', (req, res) => {
-  res.send(animals);
+app.get('/animals', (req, res, next) => {
+  req.data = animals;
+  next();
+}, limitMiddleware, (req, res) => {
+  res.json(req.data);
 })
 
 app.listen(port, () => {
